649154335773ce146ba2a81f51318a10b9780b8d
gkibria121 <gkibria121@gmail.com>
2025-03-11 22:50:49 +0600

    handle signup error

--- client/src/actions/signup.ts
+++ client/src/actions/signup.ts
@@ -1,16 +1,29 @@
 "use server";
+import axios from "axios";
 import { submitSignUpData, validateSignUpData } from "../services/authService";
-import { SignUpReturnType } from "../types/errors";
+import { AuthReturnType } from "../types/errors";
 
 // Main Sign Up Action
 export default async function signUpAction(
   formData: FormData
-): Promise<SignUpReturnType> {
+): Promise<AuthReturnType> {
   const { success, data, errors } = validateSignUpData(formData);
 
   if (!success) {
     return { message: "Validation failed!", errors };
   }
+  try {
+    const response = await submitSignUpData(data!);
 
-  return await submitSignUpData(data!);
+    // Redirect after ensuring the cookie is set
+
+    return response?.data as AuthReturnType;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response?.status === 442) {
+      return error.response.data as AuthReturnType;
+    }
+
+    console.error("SignIn Error:", error);
+    throw new Error("Sign-in failed: " + (error as Error).message);
+  }
 }
