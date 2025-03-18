eacc7757bc7af81e4a323078a3a6c34b1d5dd259
gkibria121 <gkibria121@gmail.com>
2025-03-18 13:14:13 +0600

    update script

--- bin/update_common_lib.sh
+++ bin/update_common_lib.sh
@@ -2,7 +2,27 @@
 
 command="npm i @_gktickets/common"
 removeCommand="npm remove @_gktickets/common"
-cd ./common && npm run pub && cd ../;
-cd ./auth && $removeCommand && $command && cd ../;
-cd ./client && $removeCommand && $command && cd ../;
-cd ./ticket && $removeCommand && $command && cd ../;
+
+read -p "Should publish (yes/no): " shouldPublish
+
+if [ "$shouldPublish" = "yes" ]; then
+  cd ./common || exit 1
+
+  read -p "Should commit (yes/no): " shouldCommit
+
+  if [ "$shouldCommit" = "yes" ]; then
+    git add .
+    read -p "Commit message: " commitMessage
+    git commit -m "$commitMessage"
+    npm run pub
+  fi
+
+  cd ..
+fi
+
+for dir in auth client ticket; do
+  cd "./$dir" || exit 1
+  $removeCommand
+  $command
+  cd ..
+done
