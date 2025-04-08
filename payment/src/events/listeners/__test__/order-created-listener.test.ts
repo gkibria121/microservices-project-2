7f12d111eaf8c1120a047d83a4e7def0a2c1b4a5
gkibria121 <gkibria121@gmail.com>
2025-04-08 10:02:45 +0600

    add order created listener test

--- payment/src/events/listeners/__test__/order-created-listener.test.ts
+++ payment/src/events/listeners/__test__/order-created-listener.test.ts
@@ -9,10 +9,12 @@ import mongoose from "mongoose";
 import { Message } from "node-nats-streaming";
 import { natsWrapper } from "../../../lib/natas-client";
 import { OrderCreatedListener } from "../order-created-listener";
+import Order from "../../../models/Order";
+import { createOrder } from "../../../helpers/test_helpers";
 const EXPIRATION_DURATION = 20 * 1000;
-const setup = async () => {
+const setup = async (id: string = new mongoose.mongo.ObjectId().toString()) => {
   const data: OrderCreatedEvent["data"] = {
-    id: new mongoose.mongo.ObjectId().toString(),
+    id,
     expiresAt: new Date().getTime() + EXPIRATION_DURATION + "",
     status: OrderStatus.Created,
     ticket: {
@@ -38,6 +40,27 @@ const setup = async () => {
   return { data, msg, listener };
 };
 
-it.todo("Should create order");
-it.todo("Should return error if order is already there");
-it.todo("Should Ack message.");
+it("Should create order", async () => {
+  let orderLenth = (await Order.find()).length;
+  expect(orderLenth).toBe(0);
+  const { data, msg, listener } = await setup();
+  await listener.onMessage(data, msg);
+  orderLenth = (await Order.find()).length;
+  expect(orderLenth).toBe(1);
+});
+it("Should return error if order is already there", async () => {
+  const order = await createOrder();
+  const { data, msg, listener } = await setup(order._id as string);
+
+  try {
+    await listener.onMessage(data, msg);
+  } catch (error) {
+    return;
+  }
+  throw new Error("Should test fail");
+});
+it("Should Ack message.", async () => {
+  const { data, msg, listener } = await setup();
+  await listener.onMessage(data, msg);
+  expect(msg.ack).toHaveBeenCalled();
+});
