688726d5656d3afad97f780c3cfeb52af7fded84
gkibria121 <gkibria121@gmail.com>
2025-04-08 10:08:38 +0600

    add todo

--- payment/src/routes/__test__/api.test.ts
+++ payment/src/routes/__test__/api.test.ts
@@ -9,3 +9,13 @@ describe("API Routes", () => {
     expect(response.body.message).toBe("Api endpoint not found!");
   });
 });
+
+it.todo("Should return 442 if orderId is not provided");
+it.todo("Should return 442 if token is not provided");
+it.todo("Should return 442 if orderId and token is not provided");
+it.todo("Should return error if order is not found!");
+it.todo("Should call stripe api and fail  with invalid token");
+it.todo("Should call stripe api and fail  with insufficient balance");
+it.todo("Should call stripe api and pay with token");
+it.todo("Should publish payment created event");
+it.todo("Should return 200 with  payment object");
